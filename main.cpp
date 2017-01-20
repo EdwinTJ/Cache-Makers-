1e0db044fd9d7f9e5761d3aef19d2407be3666ed
Cache Makers <Image15@iMac-GG.local>
2017-01-19 17:57:59 -0700

    Add cpp

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main()
+{
+  std:: cout <<"Welcome To The Game"<< std::endl;
+  int turn = 0;
+  bool gameOver = false;
+char board[3][3] = {{' ',' ',' '}{' ',' ',' '}{' ',' ',' '}}
+for(int i= 0; i<3; ++i ){
+  for (int j=0 j<3; ++j)
+}
+   while(gameOver == false)
+   {
+     if(turn == 0)
+     {
+       std:: cout << "it is player one's turn" << std:: endl;
+     }else{
+       std:: cout <<"it is player two's turn" << std:: endl;
+        }
+
+   //dostuff
+
+
+      if(turn == 0){
+        turn = 1;
+      }else{
+        turn= 0;
+      }
+   }
+}
