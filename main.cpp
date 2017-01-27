aacfb8bc892e9e440811f91c45eeb9ff30c665b4
Cache Makers <Image15@iMac-I.local>
2017-01-26 18:01:59 -0700

    Tictac 26

--- main.cpp
+++ main.cpp
@@ -1,16 +1,30 @@
 #include <iostream>
 
+char board[3][3] = {{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}};
+
+void printBoard(){
+
+  for(int i= 0; i<3; ++i ){
+
+    for (int j=0; j<3; ++j){
+      std::cout << board[i][j]<<"____";
+  }
+  std::cout << std:: endl;
+}
+}
+
+
 int main()
 {
   std:: cout <<"Welcome To The Game"<< std::endl;
   int turn = 0;
+  int row;
+  int col;
   bool gameOver = false;
-char board[3][3] = {{' ',' ',' '}{' ',' ',' '}{' ',' ',' '}}
-for(int i= 0; i<3; ++i ){
-  for (int j=0 j<3; ++j)
-}
+
    while(gameOver == false)
    {
+     printBoard();
      if(turn == 0)
      {
        std:: cout << "it is player one's turn" << std:: endl;
@@ -19,7 +33,15 @@ for(int i= 0; i<3; ++i ){
         }
 
    //dostuff
-
+ std:: cout <<"Number for Row"<< std:: endl;
+ std:: cin >> row;
+ std:: cout <<"Number for Column"<< std:: endl;
+ std:: cin>> col;
+ if (turn == 0) {
+   board[row][col] = 'x';
+ }else{
+   board[row][col] = 'o';
+ }
 
       if(turn == 0){
         turn = 1;
